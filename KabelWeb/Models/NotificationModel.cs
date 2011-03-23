ec7e772bda8f72bfe48fbc5d87e9265bbdbc1594
nilsr <nilsr@informi.dk>
2011-03-23 14:25:13 +0100

    added a mobile controller and views.
    now going for a jquery location enabled webapp.

---
+++ KabelWeb/Models/NotificationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KabelWeb.Models
+{
+    public class NotificationModel
+    {
+        private Point _point;
+        private string _message;
+        public Point Point
+        {
+            get { return _point; }
+            set { _point = value; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+    }
+   
+}
---
+++ KabelWeb/Models/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KabelWeb.Models
+{
+    public class Point
+    {
+        private double _x;
+        private double _y;
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public double X
+        {
+            get { return _x; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+        }
+    }
+}
